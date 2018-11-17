42737708e71b5754d4523d39392695f0ada45a66
Jankovec, Mark <mark.jankovec@here.com>
2018-11-17 14:27:32 -0600

    INIT

---
+++ src/main/java/com/jankov/HyperbolicDebugChamber/HyperbolicDebugChamberApplication.java
@@ -0,0 +1,12 @@
+package com.jankov.HyperbolicDebugChamber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperbolicDebugChamberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperbolicDebugChamberApplication.class, args);
+	}
+}
